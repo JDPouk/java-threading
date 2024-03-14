544ffd069b607aabfb7701dc92ddf409fe701c1f
kingjohn130 <31618815+kingjohn130@users.noreply.github.com>
2024-03-14 15:32:09 +1100

    (John) Create Threads with implements

--- src/com/threading/Application.java
+++ src/com/threading/Application.java
@@ -3,20 +3,40 @@ package com.threading;
 public class Application {
 	public static void main(String args[]) {
 		
-		Task taskRunner = new Task();
-		taskRunner.start();
-		System.out.println("Hi");
+		System.out.println("Starting:");
 
+		Task taskRunner = new Task("Thread-A");
+		taskRunner.run();
+		System.out.println("Thread 1");
+		
+		Task taskRunner2 = new Task("Thread-B");
+		taskRunner2.run();
+		System.out.println("Thread 2");
+		
 	}
 	
 
 }
 
 
-class Task extends Thread {
-	public void run() {
-		for(int i=0; i < 1000; i++) {
-		System.out.println("Number: " + i);
-		}
+class Task implements Runnable {
+	
+	String name;
+	
+	public Task(String name) {
+		this.name = name;
 	}
+	
+	public void run() {
+		Thread.currentThread().setName(this.name);
+		for(int i=0; i < 1000; i++) {
+		System.out.println("Number: " + i + " - " + Thread.currentThread().getName() );
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			
+		}
+
+	  }
+   }
 }
